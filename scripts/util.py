be7b1f970503119cd089d51fcf87d77a7025763c
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2020-03-23 16:33:26 -0500

    added util with plot_silhouette function

---
+++ scripts/util.py
@@ -0,0 +1,66 @@
+def plo_silhouette(n_clusters, X, reduced_data, pca):
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize = (15,5))
+    
+    ax1.set_xlim([-0.1, 1])
+    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+    clusterer = sklearn.cluster.KMeans(n_clusters=n_clusters, random_state=10)
+    cluster_labels = clusterer.fit_predict(X)
+    
+    silhouette_avg = sklearn.metrics.silhouette_score(X, cluster_labels)
+
+    # Compute the silhouette scores for each sample
+    sample_silhouette_values = sklearn.metrics.silhouette_samples(X, cluster_labels)
+
+    y_lower = 10
+    
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+        cmap = matplotlib.cm.get_cmap("nipy_spectral")
+        color = cmap(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                          0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        y_lower = y_upper + 10
+    
+    ax1.set_title("The silhouette plot for the various clusters.")
+    ax1.set_xlabel("The silhouette coefficient values")
+    ax1.set_ylabel("Cluster label")
+
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    # 2nd Plot showing the actual clusters formed
+    cmap = matplotlib.cm.get_cmap("nipy_spectral")
+    colors = cmap(float(i) / n_clusters)
+    ax2.scatter(reduced_data[:, 0], reduced_data[:, 1], marker='.', s=30, lw=0, alpha=0.7,
+                c=colors)
+
+    # Labeling the clusters
+    centers = clusterer.cluster_centers_
+    projected_centers = pca.transform(centers)
+    # Draw white circles at cluster centers
+    ax2.scatter(projected_centers[:, 0], projected_centers[:, 1],
+                marker='o', c="white", alpha=1, s=200)
+
+    for i, c in enumerate(projected_centers):
+        ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1, s=50)
+
+    ax2.set_title("The visualization of the clustered data.")
+    ax2.set_xlabel("PC 1")
+    ax2.set_ylabel("PC 2")
+
+    plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+                  "with n_clusters = %d" % n_clusters),
+                 fontsize=14, fontweight='bold')
+    plt.show()
+    print("For n_clusters = {}, The average silhouette_score is : {:.3f}".format(n_clusters, silhouette_avg))
