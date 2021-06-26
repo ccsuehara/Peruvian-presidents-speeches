2a7336700178b51c2e2496bcc7d00ae5b6a950a4
luisesanmartin <luise.sanmartin@gmail.com>
2021-06-26 18:46:25 -0400

    added word tokenize function and a more complete NLP spacy model
    - word tokenizing function is now a function from our helper script and not the function from the package lucem_illud_2020
    - also removed cell numbers from the nb conversion

--- notebooks/data_preparation/2_data_preparation.py
+++ notebooks/data_preparation/2_data_preparation.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[17]:
-
-
-#%load_ext autoreload
-#%autoreload 2
-
-
-# In[2]:
+# # Data preparation
+#
+# This notebook reads the `TXT` files of the speeches and builds a single
+# dataframe with every tokenized and normalized content we'll use.
+#
+# Please note that none of the code chunks of this notebook were actually ran
+# from here. As the processing part took a great amount of time to be completed,
+#  we transformed this notebook in a Python script and submitted it through the
+# slurm work manager of the Computer Science department. The script is the file
+# `2_data_preparation.py` in this same directory, and the file
+# `run_data_preparation.sbatch` loads it to the slurm environment.
 
 
 import pandas as pd
@@ -16,32 +19,20 @@ import re
 import spacy
 from nltk.tokenize import sent_tokenize
 
-try:
-    from lucem_illud_2020 import word_tokenize
-except ImportError:
-    get_ipython().system('pip3 install lxml --user')
-    from lucem_illud_2020 import word_tokenize
-
 import sys
 sys.path.insert(0, '../../scripts')
-from data_cleaning import *
+import data_cleaning as clean
 
 
-# In[3]:
-
-
+# Defining the directory of the speeches:
 speeches_dir = '../../data/presidentialSpeechPeru/txt'
 
-
-# In[4]:
-
-
-speeches_raw = loadcorpus(speeches_dir)
+# We load the corpus from this path using a helper function we created:
+speeches_raw = clean.loadcorpus(speeches_dir)
 
 
-# In[5]:
-
-
+# After this, we load the result in a data frame and start adding some
+# metadata columns:
 speech = pd.DataFrame()
 filenames = []
 raw = []
@@ -57,85 +48,46 @@ speech['year'] = speech['filename'].apply(lambda x: pattern.search(x).group(0))
 speech = speech.sort_values(by='year').reset_index(drop=True)
 
 
-# In[6]:
+# Now we clean these raw texts using another ad-hoc function:
+speech['cleaned text'] = speech['raw text'].apply(lambda x: clean.clean_raw_text(x))
 
 
-speech['cleaned text'] = speech['raw text'].apply(lambda x: clean_raw_text(x))
-
-
-# In[7]:
-
-
+# Adding the administration and president of each speech:
 speech.loc[(speech['year'].astype('int32') >= 1956) &            (speech['year'].astype('int32') <= 1961), 'administration'] = 'Prado'
-
 speech.loc[(speech['year'].astype('int32') >= 1962) &            (speech['year'].astype('int32') <= 1962), 'administration'] = 'Lindley'
-
 speech.loc[(speech['year'].astype('int32') >= 1963) &            (speech['year'].astype('int32') <= 1968), 'administration'] = 'Belaunde(1)'
-
 speech.loc[(speech['year'].astype('int32') >= 1969) &            (speech['year'].astype('int32') <= 1975), 'administration'] = 'Velasco'
-
 speech.loc[(speech['year'].astype('int32') >= 1976) &            (speech['year'].astype('int32') <= 1979), 'administration'] = 'Morales Bermudez'
-
 speech.loc[(speech['year'].astype('int32') >= 1980) &            (speech['year'].astype('int32') <= 1984), 'administration'] = 'Belaunde(2)'
-
 speech.loc[(speech['year'].astype('int32') >= 1985) &            (speech['year'].astype('int32') <= 1989), 'administration'] = 'Garcia(1)'
-
 speech.loc[(speech['year'].astype('int32') >= 1990) &            (speech['year'].astype('int32') <= 1994), 'administration'] = 'Fujimori(1)'
-
 speech.loc[(speech['year'].astype('int32') >= 1995) &            (speech['year'].astype('int32') <= 2000), 'administration'] = 'Fujimori(2)'
-
 speech.loc[(speech['year'].astype('int32') >= 2001) &            (speech['year'].astype('int32') <= 2005), 'administration'] = 'Toledo'
-
 speech.loc[(speech['year'].astype('int32') >= 2006) &            (speech['year'].astype('int32') <= 2010), 'administration'] = 'Garcia(2)'
-
 speech.loc[(speech['year'].astype('int32') >= 2011) &            (speech['year'].astype('int32') <= 2015), 'administration'] = 'Humala'
-
 speech.loc[(speech['year'].astype('int32') >= 2016), 'administration'] = 'Kuzcynski/Vizcarra'
-
 speech.loc[(speech['year'].astype('int32') >= 1956) &            (speech['year'].astype('int32') <= 1961), 'president'] = 'Prado'
-
 speech.loc[(speech['year'].astype('int32') >= 1962) &            (speech['year'].astype('int32') <= 1962), 'president'] = 'Lindley'
-
 speech.loc[(speech['year'].astype('int32') >= 1963) &            (speech['year'].astype('int32') <= 1968), 'president'] = 'Belaunde'
-
 speech.loc[(speech['year'].astype('int32') >= 1969) &            (speech['year'].astype('int32') <= 1975), 'president'] = 'Velasco'
-
 speech.loc[(speech['year'].astype('int32') >= 1976) &            (speech['year'].astype('int32') <= 1979), 'president'] = 'Morales Bermudez'
-
 speech.loc[(speech['year'].astype('int32') >= 1980) &            (speech['year'].astype('int32') <= 1984), 'president'] = 'Belaunde'
-
 speech.loc[(speech['year'].astype('int32') >= 1985) &            (speech['year'].astype('int32') <= 1989), 'president'] = 'Garcia'
-
 speech.loc[(speech['year'].astype('int32') >= 1990) &            (speech['year'].astype('int32') <= 2000), 'president'] = 'Fujimori'
-
 speech.loc[(speech['year'].astype('int32') >= 2001) &            (speech['year'].astype('int32') <= 2005), 'president'] = 'Toledo'
-
 speech.loc[(speech['year'].astype('int32') >= 2006) &            (speech['year'].astype('int32') <= 2010), 'president'] = 'Garcia'
-
 speech.loc[(speech['year'].astype('int32') >= 2011) &            (speech['year'].astype('int32') <= 2015), 'president'] = 'Humala'
-
 speech.loc[(speech['year'].astype('int32') >= 2016) &            (speech['year'].astype('int32') <= 2017), 'president'] = 'Kuzcynski'
-
 speech.loc[(speech['year'].astype('int32') >= 2018), 'president'] = 'Vizcarra'
-
 speech['year-president'] = speech['year'] + '-' + speech['president']
-
-
-# In[8]:
-
 
 speech.head()
 
+# Tokenizing words:
+speech['tokenized_words'] = speech['cleaned text'].apply(lambda x: clean.word_tokenize(x))
 
-# In[9]:
-
-
-speech['tokenized_words'] = speech['cleaned text'].apply(lambda x: word_tokenize(x))
-
-
-# In[10]:
-
-
+# Now we normalize words. First we add as stop words some of
+# the most frequent words:
 countsDict = {}
 for word in speech['tokenized_words'].sum():
     if word in countsDict:
@@ -145,10 +97,6 @@ for word in speech['tokenized_words'].sum():
 word_counts = sorted(countsDict.items(), key = lambda x : x[1], reverse = True)
 word_counts[:25]
 
-
-# In[11]:
-
-
 stop_words = []
 for word, count in word_counts:
     if word == 'al':
@@ -156,48 +104,25 @@ for word, count in word_counts:
     else:
         stop_words.append(word)
 
-
-# In[12]:
-
-
+# Now we are more stop words, mainly prepositions and articles:
 more_words = ['por', 'al', 'este', 'en', 'como', 'lo', 'el', 'la', 'las', 'los', 'su', 'sus'
               'y', 'de', 'que', 'a', 'del', 'para', 'con', 'se', 'ante']
 for word in more_words:
     stop_words.append(word)
 
+# Now we normalize:
+speech['normalized_words'] = speech['tokenized_words'].apply(lambda x: clean.normalize_tokens(x, stop_words))
 
-# In[13]:
-
-
-speech['normalized_words'] = speech['tokenized_words'].apply(lambda x: normalize_tokens(x, stop_words))
-
-
-# In[14]:
-
-
+# Then, we tokenize sentences using the function from `nltk` for this:
 speech['tokenized_sentences'] = speech['cleaned text'].apply(sent_tokenize)
 
+# Now we tokenize each word in each sentence:
+speech['tokenized_words_in_sentences'] = speech['tokenized_sentences'].apply(lambda x: [clean.word_tokenize(s) for s in x])
 
-# In[15]:
-
-
-speech['tokenized_words_in_sentences'] = speech['tokenized_sentences'].apply(lambda x: [word_tokenize(s) for s in x])
-
-
-# In[20]:
-
-
-speech['normalized_words_in_sentences'] = speech['tokenized_words_in_sentences'].apply(lambda x: [normalize_tokens(s, stop_words) for s in x])
-
-
-# In[ ]:
-
+# Finally, we normalized each tokenized word within each sentence:
+speech['normalized_words_in_sentences'] = speech['tokenized_words_in_sentences'].apply(lambda x: [clean.normalize_tokens(s, stop_words) for s in x])
 
 speech.head()
 
-
-# In[ ]:
-
-
+# Saving the result:
 speech.to_pickle('../../data/clean/speech.pkl')
-
