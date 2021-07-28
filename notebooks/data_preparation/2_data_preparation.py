8ea6efa238f2e4ab67ac0f1805df8a45569332f9
luisesanmartin <luise.sanmartin@gmail.com>
2021-07-27 20:18:53 -0400

    corrected spelling of Kuczynski in data prep

--- notebooks/data_preparation/2_data_preparation.py
+++ notebooks/data_preparation/2_data_preparation.py
@@ -64,7 +64,7 @@ speech.loc[(speech['year'].astype('int32') >= 1995) &            (speech['year']
 speech.loc[(speech['year'].astype('int32') >= 2001) &            (speech['year'].astype('int32') <= 2005), 'administration'] = 'Toledo'
 speech.loc[(speech['year'].astype('int32') >= 2006) &            (speech['year'].astype('int32') <= 2010), 'administration'] = 'Garcia(2)'
 speech.loc[(speech['year'].astype('int32') >= 2011) &            (speech['year'].astype('int32') <= 2015), 'administration'] = 'Humala'
-speech.loc[(speech['year'].astype('int32') >= 2016) &            (speech['year'].astype('int32') <= 2019), 'administration'] = 'Kuzcynski/Vizcarra'
+speech.loc[(speech['year'].astype('int32') >= 2016) &            (speech['year'].astype('int32') <= 2019), 'administration'] = 'Kuczynski/Vizcarra'
 speech.loc[(speech['year'].astype('int32') >= 2020) &            (speech['year'].astype('int32') <= 2020), 'administration'] = 'Sagasti'
 
 speech.loc[(speech['year'].astype('int32') >= 1956) &            (speech['year'].astype('int32') <= 1961), 'president'] = 'Prado'
@@ -78,7 +78,7 @@ speech.loc[(speech['year'].astype('int32') >= 1990) &            (speech['year']
 speech.loc[(speech['year'].astype('int32') >= 2001) &            (speech['year'].astype('int32') <= 2005), 'president'] = 'Toledo'
 speech.loc[(speech['year'].astype('int32') >= 2006) &            (speech['year'].astype('int32') <= 2010), 'president'] = 'Garcia'
 speech.loc[(speech['year'].astype('int32') >= 2011) &            (speech['year'].astype('int32') <= 2015), 'president'] = 'Humala'
-speech.loc[(speech['year'].astype('int32') >= 2016) &            (speech['year'].astype('int32') <= 2017), 'president'] = 'Kuzcynski'
+speech.loc[(speech['year'].astype('int32') >= 2016) &            (speech['year'].astype('int32') <= 2017), 'president'] = 'Kuczynski'
 speech.loc[(speech['year'].astype('int32') >= 2018) &            (speech['year'].astype('int32') <= 2019), 'president'] = 'Vizcarra'
 speech.loc[(speech['year'].astype('int32') >= 2020) &            (speech['year'].astype('int32') <= 2020), 'president'] = 'Sagasti'
 
